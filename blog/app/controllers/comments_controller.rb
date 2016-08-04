97317912d2b32b7a42c6dd93b83a6dcb3f96bd11
baurine <2008.hbl@gmail.com>
2016-08-04 15:06:09 +0800

    enable delete comments

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -12,6 +12,14 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  # DELETE
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   
 end
