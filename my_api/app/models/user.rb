36139308c88bce4636c38a45c1ab051aeb0f1b5a
baurine <2008.hbl@gmail.com>
2016-08-04 17:57:22 +0800

    add authentication

--- my_api/app/models/user.rb
+++ my_api/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+
+  before_create do |user|
+    user.api_key = user.generate_api_key
+  end
+
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token)
+    end
+  end
+
 end
