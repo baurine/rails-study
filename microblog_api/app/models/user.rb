23986756642edb6be10d78791e3d6a87e9661bbc
baurine <2008.hbl@gmail.com>
2016-08-04 19:02:42 +0800

    add auth token for user

--- microblog_api/app/models/user.rb
+++ microblog_api/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+
+  before_create :generate_auth_token
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64).tr('+/=', 'Qrt')
+      break unless User.exists?(auth_token: auth_token)
+    end
+  end
+
+  def reset_auth_token
+    generate_auth_token
+    save
+  end
+
 end
--- microblog_api/db/schema.rb
+++ microblog_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804102828) do
+ActiveRecord::Schema.define(version: 20160804104249) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160804102828) do
     t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "auth_token"
   end
 
 end
