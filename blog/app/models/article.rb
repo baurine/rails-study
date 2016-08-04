33a8c1dad794e27a67c9f14e2d822880301672f3
baurine <2008.hbl@gmail.com>
2016-08-04 14:36:06 +0800

    create and display comments for an article

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
   
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root 'welcome#index'
 end
