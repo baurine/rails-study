8efbf71711ec75168e8490d13bf592af81fc3c82
baurine <2008.hbl@gmail.com>
2016-08-04 12:07:16 +0800

    implement Article part
    include controller, model, view, now can create/show/update/delete articles

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+  # GET
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # POST
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      # render 'new' is to render new.html.erb, won't call new action
+      render 'new'
+    end
+  end
+
+  # PATCH / PUT
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
---
+++ blog/app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+
+  validates :title, presence: true, length: { minimum: 5 }
+  
+end
