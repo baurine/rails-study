3734b8beb075d30aea097b4ae437d887b648798a
baurine <2008.hbl@gmail.com>
2016-08-05 12:33:58 +0800

    handle session

--- microblog_api/app/controllers/application_controller.rb
+++ microblog_api/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  attr_accessor :current_user
+
+  def api_error(opts = {})
+    render head: :unauthorized, status: opts[:status]
+  end
 end
--- microblog_api/config/routes.rb
+++ microblog_api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources :sessions, only: [:create]
+  post 'login', to: 'sessions#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
