213bd87d296656fb667972bf8352603c9bf36679
baurine <2008.hbl@gmail.com>
2016-08-08 11:26:13 +0800

    test contact page and implement it

--- rails_tutorial/micropost_web/test/controllers/static_pages_controller_test.rb
+++ rails_tutorial/micropost_web/test/controllers/static_pages_controller_test.rb
@@ -24,4 +24,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@baseTitle}"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@baseTitle}"
+  end
+
 end
