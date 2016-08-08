26219ae04b65ba5bb6801ff771a8a5c32a4920dc
baurine <2008.hbl@gmail.com>
2016-08-08 15:01:25 +0800

    use named routes

--- rails_tutorial/micropost_web/config/routes.rb
+++ rails_tutorial/micropost_web/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   root 'static_pages#home'
+  # get '/help',  to:   'static_pages#help', as: 'helf'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
