df4bb6eac4b9bbb5d6924da9a1a4a1ef68a54777
baurine <2008.hbl@gmail.com>
2016-08-08 17:14:34 +0800

    add password for User model

--- rails_tutorial/micropost_web/test/models/user_test.rb
+++ rails_tutorial/micropost_web/test/models/user_test.rb
@@ -6,7 +6,10 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", 
+                     email: "user@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -69,6 +72,16 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
