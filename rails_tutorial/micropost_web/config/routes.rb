8f56068ed86037d5071726a00f97d6a735fb7fc2
baurine <2008.hbl@gmail.com>
2016-08-08 11:30:58 +0800

    set static_pages/home as root path

--- rails_tutorial/micropost_web/config/routes.rb
+++ rails_tutorial/micropost_web/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  # root 'application#hello'
+  root 'static_pages#home'
 end
--- rails_tutorial/micropost_web/test/controllers/static_pages_controller_test.rb
+++ rails_tutorial/micropost_web/test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @baseTitle = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get root_url
+    assert_select "title", "Home | #{@baseTitle}"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
