bad4bee348cbaf50d9b260e777240082939c1c74
baurine <2008.hbl@gmail.com>
2016-08-09 18:59:25 +0800

    fake 100 users

--- rails_tutorial/micropost_web/db/seeds.rb
+++ rails_tutorial/micropost_web/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
