d99674205775b235b7b4abd98d1d07e78600fe21
baurine <2008.hbl@gmail.com>
2016-08-09 11:52:24 +0800

    display flash messages while signup success

--- rails_tutorial/micropost_web/app/controllers/users_controller.rb
+++ rails_tutorial/micropost_web/app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      flash[:success] = "Welcome to Rails Sample App"
       redirect_to @user
     else
       render 'new'
