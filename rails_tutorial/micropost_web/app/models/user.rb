21938784711b5397bcb82fd6405ffa8ecca2f0ab
baurine <2008.hbl@gmail.com>
2016-08-12 19:17:30 +0800

    create active relationship

--- rails_tutorial/micropost_web/app/models/user.rb
+++ rails_tutorial/micropost_web/app/models/user.rb
@@ -10,8 +10,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   attr_accessor :remember_token
 
-  # releation with microposts
+  # relation with microposts
   has_many :microposts, dependent: :destroy
+
+  # relation with follower
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   def remember
     self.remember_token = User.new_token
