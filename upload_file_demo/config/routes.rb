bdde8fc30993c40d24fe689dc12146ef5a4b6bdb
baurine <2008.hbl@gmail.com>
2017-03-30 16:20:20 +0800

    Implement upload file by carrierwave

--- upload_file_demo/config/routes.rb
+++ upload_file_demo/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :resumes, only: [:index, :new, :create, :destroy]
+  root 'resumes#index'
 end
