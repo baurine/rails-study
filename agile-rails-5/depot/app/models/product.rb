03386c73e17c023d062276e93275995d3b905881
baurine <2008.hbl@gmail.com>
2017-04-30 21:43:17 +0800

    Connect Cart and Product to LineItem

--- agile-rails-5/depot/app/models/product.rb
+++ agile-rails-5/depot/app/models/product.rb
@@ -6,4 +6,15 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
