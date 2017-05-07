b5617c8675869ae11a454ffa173bc114c584d55f
baurine <2008.hbl@gmail.com>
2017-05-07 23:00:50 +0800

    Chapter 15 - Selecting the locale

--- agile-rails-5/depot/app/controllers/application_controller.rb
+++ agile-rails-5/depot/app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   protected
     def authorize
       unless User.find_by(id: session[:user_id])
         redirect_to login_url, notice: 'Please login'
+      end
+    end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
       end
     end
 end
--- agile-rails-5/depot/config/routes.rb
+++ agile-rails-5/depot/config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-
   controller :sessions do
     get     'login'  => :new
     post    'login'  => :create
     delete  'logout' => :destroy
   end
 
-  resources :orders
-  resources :line_items
-  resources :carts
-
+  resources :users
   resources :products do
     get :who_bought, on: :member
   end
 
-  resources :users
-
-  root 'store#index', as: 'store_index'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
