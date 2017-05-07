bbf6da349b8de584530148a26d9870b48e96a207
baurine <2008.hbl@gmail.com>
2017-05-07 08:22:40 +0800

    Chapter 12 - Support atom feed

--- agile-rails-5/depot/app/models/product.rb
+++ agile-rails-5/depot/app/models/product.rb
@@ -8,6 +8,8 @@ class Product < ApplicationRecord
   }
 
   has_many :line_items
+  has_many :orders, through: :line_items
+
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
--- agile-rails-5/depot/config/routes.rb
+++ agile-rails-5/depot/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
 end
