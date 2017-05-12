a17d3c5def477b8e91941fefcc9a0216b5b337f2
baurine <2008.hbl@gmail.com>
2017-05-12 19:12:42 +0800

    Chapter 15 - Translation

--- agile-rails-5/depot/app/controllers/orders_controller.rb
+++ agile-rails-5/depot/app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_index_url, notice: t('thanks') }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
