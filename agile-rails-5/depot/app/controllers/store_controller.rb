c6d9b54d28959c9d258545aee0177d278515e29c
baurine <2008.hbl@gmail.com>
2017-05-12 19:25:36 +0800

    Chapter 15 - Add locale switcher

--- agile-rails-5/depot/app/controllers/store_controller.rb
+++ agile-rails-5/depot/app/controllers/store_controller.rb
@@ -4,6 +4,10 @@ class StoreController < ApplicationController
   skip_before_action :authorize
 
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
 end
